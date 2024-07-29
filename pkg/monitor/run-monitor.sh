2f53851a6186845ddc4e643c6a57843320ceb51a
Mikhail Malyshev <mike.malyshev@gmail.com>
2024-07-29 07:51:30 +0000

    Disable dmesg except for panic and add proper keymap for CTRL + left/right
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- pkg/monitor/run-monitor.sh
+++ pkg/monitor/run-monitor.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 echo "Running EVE monitor"
-openvt -c 1 -f -s -w -- /sbin/monitor
+env RUST_BACKTRACE=1
+
+# leave only panic on console
+dmesg -n 1
+
+# setup keymap
+loadkeys -s us - <<EOF
+control keycode 103 = F103
+control keycode 108 = F108
+control keycode 106 = F106
+control keycode 105 = F105
+string F103 = "\033[1;5A"
+string F108 = "\033[1;5B"
+string F106 = "\033[1;5C"
+string F105 = "\033[1;5D"
+EOF
+
+openvt -c 2 -s -f -w -- /sbin/monitor
