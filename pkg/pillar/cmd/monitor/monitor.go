437aef1df6a09599f58f7ecadf575635977ef966
Mikhail Malyshev <mike.malyshev@gmail.com>
2024-10-04 18:22:12 +0200

    Add function to update /config/server
    Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

--- pkg/pillar/cmd/monitor/monitor.go
+++ pkg/pillar/cmd/monitor/monitor.go
@@ -4,11 +4,17 @@
 package monitor
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/lf-edge/eve/pkg/pillar/agentbase"
 	"github.com/lf-edge/eve/pkg/pillar/base"
 	"github.com/lf-edge/eve/pkg/pillar/pubsub"
+	"github.com/lf-edge/eve/pkg/pillar/types"
 	"github.com/sirupsen/logrus"
 )
 
@@ -28,8 +34,62 @@ type monitorContext struct {
 	subscriptions       map[string]pubsub.Subscription
 	pubDevicePortConfig pubsub.Publication
 	clientConnected     chan bool
+	serverNameAndPort   string
 
 	IPCServer *IPCServer
+}
+
+func (ctx *monitorContext) readServerFile() error {
+	if _, err := os.Stat(types.ServerFileName); os.IsNotExist(err) {
+		ctx.serverNameAndPort = ""
+		return nil
+	}
+	server, err := os.ReadFile(types.ServerFileName)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	ctx.serverNameAndPort = strings.TrimSpace(string(server))
+	return nil
+}
+
+func (ctx *monitorContext) updateServerFile(newServer string) error {
+	// 1. Find the CONFIG partition
+	findCmd := exec.Command("/sbin/findfs", "PARTLABEL=CONFIG")
+	deviceBytes, err := findCmd.Output()
+	if err != nil {
+		return fmt.Errorf("failed to find CONFIG partition: %v", err)
+	}
+	devicePath := strings.TrimSpace(string(deviceBytes))
+
+	// 2. Create temp directory under /run
+	tempDir, err := os.MkdirTemp("/run", "config-mount-")
+	if err != nil {
+		return fmt.Errorf("failed to create temp directory: %v", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	// 3. Mount the CONFIG partition as read-write
+	mountCmd := exec.Command("mount", "-t", "vfat", "-o", "rw,iocharset=iso8859-1", devicePath, tempDir)
+	if err := mountCmd.Run(); err != nil {
+		return fmt.Errorf("failed to mount CONFIG partition: %v", err)
+	}
+	defer exec.Command("umount", tempDir).Run()
+
+	// 4. Write new server file
+	tempServerFile := filepath.Join(tempDir, filepath.Base(types.ServerFileName))
+	if err := os.WriteFile(tempServerFile, []byte(newServer), 0644); err != nil {
+		return fmt.Errorf("failed to write new server file: %v", err)
+	}
+
+	// 5. Unmount temp directory (handled by defer)
+
+	// 6. Update shadow copy in /config
+	if err := os.WriteFile(types.ServerFileName, []byte(newServer), 0644); err != nil {
+		return fmt.Errorf("failed to update shadow copy: %v", err)
+	}
+	ctx.serverNameAndPort = newServer
+	return nil
 }
 
 func newMonitorContext() *monitorContext {
@@ -54,6 +114,10 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 		agentbase.WithBaseDir(baseDir),
 		agentbase.WithWatchdog(ps, warningTime, errorTime),
 		agentbase.WithArguments(arguments))
+
+	if err = ctx.readServerFile(); err != nil {
+		log.Fatalf("Server file exists but cannot be read `%v`", err)
+	}
 
 	if err = ctx.startIPCServer(); err != nil {
 		log.Fatalf("Cannot start Monitor IPC server `%v`", err)
